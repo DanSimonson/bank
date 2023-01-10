4ea437599775643ae311a424b87d404255687a7b
daniel simonson <dansimonson@mariposaweb.net>
2023-01-09 20:03:09 -0600

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchUsersComponent } from './components/search-users/search-users.component';
+import { CurrencyConverterComponent } from './components/currency-converter/currency-converter.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search-users', component: SearchUsersComponent },
+  { path: 'currency-conversion', component: CurrencyConverterComponent },
+  {path: '', redirectTo: '/search-users', pathMatch: 'full' },
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchUsersComponent } from './components/search-users/search-users.component';
+import { CurrencyConverterComponent } from './components/currency-converter/currency-converter.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchUsersComponent
+    SearchUsersComponent,
+    CurrencyConverterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
