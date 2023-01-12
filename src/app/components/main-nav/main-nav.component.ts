eb4ccaad00090e3447338b06e985d9691f8d3050
daniel simonson <dansimonson@mariposaweb.net>
2023-01-12 12:51:32 -0600

    added ngClass to toggle class and open menu items on small screens on hamburger button click

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,15 +1,15 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  styleUrls: ['./main-nav.component.css'],
 })
 export class MainNavComponent {
-
-  toggleMe(){
-    console.log('clickedMe: ')
-
+  toggle = false;
+  toggleMe() {
+    console.log('clickedMe: ');
+    this.toggle = !this.toggle
   }
-
 }
